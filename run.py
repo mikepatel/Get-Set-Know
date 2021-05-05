172af2882960e36eee074b8e8594c5784ea294e9
Michael <michael.patel94@gmail.com>
2021-05-05 13:05:58 -0400

    Adding getter/setter

---
+++ run.py
@@ -0,0 +1,42 @@
+"""
+Michael Patel
+May 2021
+
+Project description:
+
+File description:
+
+"""
+################################################################################
+# Imports
+
+
+################################################################################
+# Player object
+class Player:
+    def __init__(self, position="Guard"):
+        self._position = position
+
+    @property  # using property decorator
+    # getter
+    def position(self):
+        return self._position
+
+    # setter
+    @position.setter
+    def position(self, position):
+        self._position = position
+
+    # deleter
+    @position.deleter
+    def position(self):
+        del self._position
+
+
+################################################################################
+# Main
+if __name__ == "__main__":
+    me = Player()
+    print(me.position)  # get
+    me.position = "Forward"  # set
+    print(me.position)
